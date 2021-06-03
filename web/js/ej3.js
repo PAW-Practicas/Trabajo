2e8740c99172fd633ce5f954be61e14c812ecbd7
hesolar <hesolar@CCTL136-01.unirioja.loc>
2021-06-03 16:07:22 +0200

    trabajo ojo resuelto

--- web/js/ej3.js
+++ web/js/ej3.js
@@ -3,14 +3,23 @@ window.addEventListener("load",ej3);
 function ej3(){
     var eye=document.querySelectorAll(".verContr");
     eye.forEach(x => x.addEventListener("click", e => {
+        
+        var padre=e.currentTarget.parentNode
+        var textbox=padre.getElementsByTagName("input").item(0)
+        
         if(e.currentTarget.firstChild.className === "fas fa-eye-slash") {
+        
+            
         e.currentTarget.firstChild.className = "fas fa-eye";
-        e.currentTarget.type = "text";
+        
+        
+        
+        textbox.type = "text";
         }
         else 
             {
             e.currentTarget.firstChild.className = "fas fa-eye-slash";
-            e.currentTarget.type = "password";
+            textbox.type = "password";
         }
         e.preventDefault();
     }));   
